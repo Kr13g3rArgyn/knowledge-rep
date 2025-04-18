3242b42115c70275d9136cd13d893cd7b4d5a4a6
Elnaz <almasuly.elnaz@gmail.com>
2025-04-18 15:57:54 +0500

    Initial Commit

---
+++ base-proxy/src/test/java/kz/proxy/Smart_proxy/SmartProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.proxy.Smart_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
